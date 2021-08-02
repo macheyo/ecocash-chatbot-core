f7ce5129f684b8a449e310d415b4ee6df8384c20
Kudzai Macheyo <justicemacheyo@gmail.com>
2021-08-02 14:07:21 +0200

    Initial commit

---
+++ src/main/java/zw/co/cassavasmartech/ecocashchatbotcore/EcocashChatbotCoreApplication.java
@@ -0,0 +1,13 @@
+package zw.co.cassavasmartech.ecocashchatbotcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocashChatbotCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocashChatbotCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/cassavasmartech/ecocashchatbotcore/EcocashChatbotCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.cassavasmartech.ecocashchatbotcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocashChatbotCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
