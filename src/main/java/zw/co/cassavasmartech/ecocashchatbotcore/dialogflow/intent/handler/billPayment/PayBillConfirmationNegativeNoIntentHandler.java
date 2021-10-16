bae8945c6f868b596616b3b9bcab0f6bd8c6c4b9
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-10-16 09:29:31 +0200

    code cleanup

--- src/main/java/zw/co/cassavasmartech/ecocashchatbotcore/dialogflow/intent/handler/billPayment/PayBillConfirmationNegativeNoIntentHandler.java
+++ src/main/java/zw/co/cassavasmartech/ecocashchatbotcore/dialogflow/intent/handler/billPayment/PayBillConfirmationNegativeNoIntentHandler.java
@@ -2,20 +2,17 @@ package zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.intent.handler.billP
 
 import lombok.extern.slf4j.Slf4j;
 import zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.DialogFlowUtil;
+import zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.data.OutputContext;
 import zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.data.WebhookRequest;
 import zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.data.WebhookResponse;
 import zw.co.cassavasmartech.ecocashchatbotcore.dialogflow.intent.IntentHandlerAdapter;
-import zw.co.cassavasmartech.ecocashchatbotcore.model.Customer;
 import zw.co.cassavasmartech.ecocashchatbotcore.model.UseCase;
+
 @Slf4j
 public class PayBillConfirmationNegativeNoIntentHandler extends IntentHandlerAdapter {
     @Override
     public WebhookResponse getWebhookResponse(WebhookRequest... webhookRequest) {
         log.info("Processing dialogflow intent: {}", webhookRequest[0].getQueryResult().getIntent().getDisplayName());
-        Customer customer = DialogFlowUtil.isNewCustomer(webhookRequest[0]);
-        return DialogFlowUtil.getResponse(webhookRequest[0],
-                DialogFlowUtil.promptProcessor(17,webhookRequest[0],customer),
-                new Object[]{},
-                UseCase.BILL_PAYMENT);
+        return DialogFlowUtil.getResponse(webhookRequest[0], DialogFlowUtil.promptProcessor(17,webhookRequest[0],DialogFlowUtil.isNewCustomer(webhookRequest[0])), new Object[]{OutputContext.builder().lifespanCount(1).name(webhookRequest[0].getSession() + "/contexts/awaiting_pay_bill_more").build()}, UseCase.BILL_PAYMENT);
     }
 }
